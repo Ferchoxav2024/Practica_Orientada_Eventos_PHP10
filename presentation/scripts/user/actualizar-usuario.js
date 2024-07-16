6cfff70fa61d55aabf0229aafc757bfec8e91217
DannyArroba <danny.gamer11@hotmail.com>
2024-07-16 13:16:10 -0500

    nosesitabien

--- presentation/scripts/user/actualizar-usuario.js
+++ presentation/scripts/user/actualizar-usuario.js
@@ -1,3 +1,4 @@
+console.log("hola")
 window.addEventListener('message', (event) => {
     const usuario = event.data;
     document.getElementById('cedula').value = usuario.cedula;
@@ -9,7 +10,56 @@ window.addEventListener('message', (event) => {
     document.getElementById('perfil').value = usuario.perfil;
   });
   
+  async function actualizarUsuario(event) {window.addEventListener('message', (event) => {
+    const usuario = event.data;
+    document.getElementById('cedula').value = usuario.cedula;
+    document.getElementById('firstName').value = usuario.firstName;
+    document.getElementById('lastName').value = usuario.lastName;
+    document.getElementById('email').value = usuario.email;
+    document.getElementById('password').value = usuario.password;
+    document.getElementById('telefono').value = usuario.telefono;
+    document.getElementById('perfil').value = usuario.perfil;
+  });
+  
   async function actualizarUsuario(event) {
+    event.preventDefault();
+    
+    const cedula = document.getElementById('cedula').value;
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const telefono = document.getElementById('telefono').value;
+    const perfil = document.getElementById('perfil').value;
+
+    const usuario = {
+        cedula: cedula,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        telefono: telefono,
+        perfil: perfil,
+    };
+
+    console.log(usuario)
+  
+    try {
+        const response = await fetch('http://localhost/Practica_Orientada_Eventos_PHP10/businessLogic/swCategoria.php', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuario)
+        });
+        window.close();
+        
+    } catch (error) {
+        console.error('Error al actualizar usuario:', error);
+    }
+}
+  
+  document.getElementById('actualizarUsuarioForm').addEventListener('submit', actualizarUsuario);
     event.preventDefault();
     
     const cedula = document.getElementById('cedula').value;
